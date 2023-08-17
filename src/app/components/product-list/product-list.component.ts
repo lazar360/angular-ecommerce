d77a0c02c4903c6534501582c3a11d63425eb0db
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-08-17 09:46:13 +0200

    search product by category version 1

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,26 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list-grid.component.html',
-  // templateUrl: './product-list-table.component.html',
-  //  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
-
 export class ProductListComponent implements OnInit {
- 
-  products : Product[] = []; 
-  constructor(private productService : ProductService){}
- 
+  products: Product[] = [];
+  currentCategoryId: number = 1;
+
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
+
   ngOnInit(): void {
-    this.listProducts();
+    this.route.params.subscribe(() => {
+      this.listProducts();
+    });
   }
 
   listProducts() {
-    this.productService.getProductList().subscribe(data => {
+    // check if id parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      // get the id and convert string to a number
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    } else {
+      // not a category id available ... return 1
+      this.currentCategoryId = 1;
+    }
+    // now get the product from the category id
+    this.productService.getProductList(this.currentCategoryId).subscribe((data) => {
       this.products = data;
     });
   }
