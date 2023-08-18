2fd1b5b0351c65bd516037ba01039559419f6653
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-08-18 15:37:40 +0200

    maj list

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -118,4 +118,8 @@ export class ProductListComponent implements OnInit {
       this.theTotalElements = data.page.totalElements;
     };
   }
+
+  addToCart(theProduct: Product){
+    console.log(`Adding to cart : ${theProduct.name}, ${theProduct.unitPrice}`);
+  }
 }
