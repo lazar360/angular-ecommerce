2d1eb2234254579bfaba60a3b13525c35d2ee181
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-08-18 16:31:31 +0200

    maj cart service

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,9 +1,51 @@
 import { Injectable } from '@angular/core';
+import { CartItem } from '../common/cart-item';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
+  cartItems: CartItem[] = [];
+
+  totalPrice: Subject<number> = new Subject<number>();
+  totalQuantity: Subject<number> = new Subject<number>();
+
   constructor() { }
+
+  addToCart(theCartItem: CartItem){
+
+    // check if we already have an item in our cart
+    let alreadyExistsInCart: boolean = false;
+    let existingCartItem: CartItem | undefined = undefined;
+
+    if(this.cartItems.length > 0){
+      // find the item in the cart based on item id 
+      for(let tempCartItem of this.cartItems){
+        if(tempCartItem.id === theCartItem.id){
+          existingCartItem = tempCartItem;
+          break
+        }
+      }
+
+      // check if we found it
+      alreadyExistsInCart = (existingCartItem != undefined);
+    }
+
+    if(alreadyExistsInCart && existingCartItem){
+      // increment the quantity
+      existingCartItem.quantity++;
+    } else {
+      this.cartItems.push(theCartItem);
+    }
+
+    // compute cart total price and quantity
+    this.computeCartTotals();
+
+  }
+  
+  computeCartTotals() {
+    // TODO : Method to implement
+  }
 }
