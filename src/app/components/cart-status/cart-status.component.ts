0970831d551d60f45d5c077945ce936e456d2334
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-08-20 12:28:23 +0200

    maj cart status component

--- src/app/components/cart-status/cart-status.component.ts
+++ src/app/components/cart-status/cart-status.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart-status',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class CartStatusComponent {
 
+  totalPrice: number = 0.00;
+  totalQuantity: number = 0;
+
+  constructor (private cartService: CartService){}
+
+  ngOnInit() {
+    this.updateCartStatus();
+  }
+
+  updateCartStatus() {
+    // suscribe to the total price
+    
+
+    // suscribe to the total quantity
+
+  }
 }
