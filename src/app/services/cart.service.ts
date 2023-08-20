c6cf1bfa9790a6c4d9eecb8ebb207039afbc6cfc
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-08-20 12:01:24 +0200

    maj cart service

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -3,37 +3,35 @@ import { CartItem } from '../common/cart-item';
 import { Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
-
   cartItems: CartItem[] = [];
 
   totalPrice: Subject<number> = new Subject<number>();
   totalQuantity: Subject<number> = new Subject<number>();
 
-  constructor() { }
+  constructor() {}
 
-  addToCart(theCartItem: CartItem){
-
+  addToCart(theCartItem: CartItem) {
     // check if we already have an item in our cart
     let alreadyExistsInCart: boolean = false;
     let existingCartItem: CartItem | undefined = undefined;
 
-    if(this.cartItems.length > 0){
-      // find the item in the cart based on item id 
-      for(let tempCartItem of this.cartItems){
-        if(tempCartItem.id === theCartItem.id){
+    if (this.cartItems.length > 0) {
+      // find the item in the cart based on item id
+      for (let tempCartItem of this.cartItems) {
+        if (tempCartItem.id === theCartItem.id) {
           existingCartItem = tempCartItem;
-          break
+          break;
         }
       }
 
       // check if we found it
-      alreadyExistsInCart = (existingCartItem != undefined);
+      alreadyExistsInCart = existingCartItem != undefined;
     }
 
-    if(alreadyExistsInCart && existingCartItem){
+    if (alreadyExistsInCart && existingCartItem) {
       // increment the quantity
       existingCartItem.quantity++;
     } else {
@@ -42,10 +40,36 @@ export class CartService {
 
     // compute cart total price and quantity
     this.computeCartTotals();
-
   }
-  
+
   computeCartTotals() {
-    // TODO : Method to implement
+    let totalPriceValue: number = 0;
+    let totalQuantityValue: number = 0;
+
+    this.cartItems.forEach((currentCartItem) => {
+      totalQuantityValue += currentCartItem.quantity;
+      totalPriceValue += currentCartItem.quantity * currentCartItem.unitPrice;
+    });
+
+    // publish the new values ... all suscribers will receive the new data
+    this.totalPrice.next(totalPriceValue);
+    this.totalQuantity.next(totalQuantityValue);
+
+    // log cart data for debugging purposes
+    this.logCartData(totalQuantityValue, totalPriceValue);
+  }
+
+  logCartData(totalQuantityValue: number, totalPriceValue: number) {
+    console.log('1/ Contents of the cart');
+    this.cartItems.forEach((cartItem) => {
+      console.log(`name : ${cartItem.name} :
+      - price : ${cartItem.unitPrice}, quantity : ${cartItem.quantity}
+      subtotal : ${cartItem.unitPrice * cartItem.quantity}
+      --------------------------------`);
+    });
+
+    console.log(`computeCartTotals :
+      - TotalQuantityValue : ${totalQuantityValue} ; 
+      - TotalPriceValue : ${totalPriceValue.toFixed(2)} ;`);
   }
 }
