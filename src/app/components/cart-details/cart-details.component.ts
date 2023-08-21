5cdd5f38ea388b3addd46ddea7b6c67ce852d476
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-08-21 10:39:14 +0200

    maj cart-details ts & html

--- src/app/components/cart-details/cart-details.component.ts
+++ src/app/components/cart-details/cart-details.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { CartItem } from 'src/app/common/cart-item';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart-details',
   templateUrl: './cart-details.component.html',
-  styleUrls: ['./cart-details.component.css']
+  styleUrls: ['./cart-details.component.css'],
 })
 export class CartDetailsComponent {
+  cartItems: CartItem[] = [];
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
 
+  constructor(private cartService: CartService) {}
+
+  ngOnInit() {
+    this.listCartDetails();
+  }
+
+  listCartDetails() {
+    this.cartItems = this.cartService.cartItems;
+
+    this.cartService.totalPrice.subscribe((data) => {
+      this.totalPrice = data;
+    });
+
+    this.cartService.totalQuantity.subscribe((data) => {
+      this.totalQuantity = data;
+    });
+
+    this.cartService.computeCartTotals(); // le next permet de publier les variables (pas besoin de les créer)
+  }
 }
