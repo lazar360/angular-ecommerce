d6ae85e950c0cb0115ea21a75b93fb0a0194195a
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-08-22 15:53:01 +0200

    custom validators

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -8,6 +8,7 @@ import {
 import { Luv2ShopFormService } from 'src/app/services/luv2-shop-form.service';
 import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
+import { Luv2ShopValidators } from 'src/app/validators/luv2-shop-validators';
 
 @Component({
   selector: 'app-checkout',
@@ -35,10 +36,12 @@ export class CheckoutComponent implements OnInit {
         firstName: new FormControl('', [
           Validators.required,
           Validators.minLength(2),
+          Luv2ShopValidators.notOnlyWhitespace
         ]),
         lastName: new FormControl('', [
           Validators.required,
           Validators.minLength(2),
+          Luv2ShopValidators.notOnlyWhitespace
         ]),
         email: new FormControl('', [
           Validators.required,
--- src/app/validators/luv2-shop-validators.ts
+++ src/app/validators/luv2-shop-validators.ts
@@ -1,2 +1,13 @@
+import { FormControl, ValidationErrors } from '@angular/forms';
+
 export class Luv2ShopValidators {
+  // whitespace validation
+  static notOnlyWhitespace(control: FormControl): ValidationErrors | null {
+    // check if the string only contains whitespace
+    if (control.value != null && control.value.trim().length === 0) {
+      // invalid , return erro object
+      return { notOnlyWhitespace: true };
+    } else {
+    return null;}
+  }
 }
