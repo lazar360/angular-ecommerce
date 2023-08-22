8ccb569c9ec13907d2f2ea2bb71c6c1d35b70244
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-08-22 13:59:25 +0200

    fix minor bug

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -85,8 +85,14 @@ export class CheckoutComponent implements OnInit {
       this.checkoutFormGroup.controls['billingAddress'].setValue(
         this.checkoutFormGroup.controls['shippingAddress'].value
       );
+
+      this.billingAddressStates = this.shippingAddressStates;
+
     } else {
       this.checkoutFormGroup.controls['billingAddress'].reset();
+
+      this.billingAddressStates = [];
+
     }
   }
 
