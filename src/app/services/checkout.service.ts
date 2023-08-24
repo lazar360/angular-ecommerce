09929ee4d17bcf48b826e0e9082ba64c5a3d401f
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-08-24 11:52:09 +0200

    maj checkout service : fix bug

--- src/app/services/checkout.service.ts
+++ src/app/services/checkout.service.ts
@@ -1,18 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Purchase } from '../common/purchase';
 import { Observable } from 'rxjs';
+import { Purchase } from '../common/purchase';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CheckoutService {
 
-  private purchaseUrl = 'https://localhost:8080/checkout/purchase';
+  private purchaseUrl = 'http://localhost:8080/api/checkout/purchase';
 
   constructor(private httpClient: HttpClient) { }
 
-  placeOrder(purchase:Purchase): Observable<any>{
-    return this.httpClient.post<Purchase>(this.purchaseUrl, purchase);
+  placeOrder(purchase: Purchase): Observable<any> {
+    return this.httpClient.post<Purchase>(this.purchaseUrl, purchase);    
   }
+  
 }
