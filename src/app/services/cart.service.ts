4f1d94a024abe7b7d54cd17dc80ff3d9555d908d
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-08-25 14:41:30 +0200

    update cartservice : add itmes to local storage

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -7,10 +7,18 @@ import { BehaviorSubject, Subject } from 'rxjs';
 })
 export class CartService {
   cartItems: CartItem[] = [];
-
   totalPrice: Subject<number> = new BehaviorSubject<number>(0);
   totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
+  storage: Storage = sessionStorage;
 
+  constructor() {
+    // read data from storage
+    let data = JSON.parse(this.storage.getItem('cartItems')!);
+    if (data != null) {
+      this.cartItems = data;
+      // compute totals based on the data that is read from storage
+    }
+  }
   addToCart(theCartItem: CartItem) {
     let existingCartItem: CartItem | undefined = undefined;
 
@@ -42,6 +50,13 @@ export class CartService {
 
     // log cart data for debugging purposes
     this.logCartData(totalQuantityValue, totalPriceValue);
+
+    // persist cart data
+    this.persistCartItems();
+  }
+
+  persistCartItems() {
+    this.storage.setItem('cartItems', JSON.stringify(this.cartItems));
   }
 
   logCartData(totalQuantityValue: number, totalPriceValue: number) {
