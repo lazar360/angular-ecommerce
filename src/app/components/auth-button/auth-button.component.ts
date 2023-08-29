6fe62da7f49ff68ae0f86f4b22c4254064ddb8bb
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-08-29 10:24:10 +0200

    create btn orderHistory & mail in sessionStorage

--- src/app/components/auth-button/auth-button.component.ts
+++ src/app/components/auth-button/auth-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 import { DOCUMENT } from '@angular/common';
 
@@ -6,17 +6,42 @@ import { DOCUMENT } from '@angular/common';
   selector: 'app-auth-button',
   template: `
     <ng-container *ngIf="auth.isAuthenticated$ | async; else loggedOut">
-      <button class="btn btn-primary" (click)="auth.logout({ logoutParams: { returnTo: document.location.origin } })">
+      <button
+        style="margin-right: 4px;"
+        class="btn btn-primary"
+        (click)="
+          auth.logout({ logoutParams: { returnTo: document.location.origin } })
+        "
+      >
         Log out
+      </button>
+
+      <button class="btn btn-primary" routerLink="/order-history">
+        Orders
       </button>
     </ng-container>
 
     <ng-template #loggedOut>
-      <button class="btn btn-primary" (click)="auth.loginWithRedirect()">Log in</button>
+      <button class="btn btn-primary" (click)="auth.loginWithRedirect()">
+        Log in
+      </button>
     </ng-template>
   `,
   styles: [],
 })
-export class AuthButtonComponent {
-  constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService) {}
+export class AuthButtonComponent implements OnInit {
+  theEmail: any = '';
+  storage: Storage = sessionStorage;
+  profileJson: string = null!;
+
+  constructor(
+    @Inject(DOCUMENT) public document: Document,
+    public auth: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.auth.user$.subscribe((profile) =>
+      this.storage.setItem('userEmail', JSON.stringify(profile?.email))
+    );
+  }
 }
